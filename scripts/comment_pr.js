e740e77b547c11019e0d5fccae7ca912ef8e650b
Nikola Grabovica <nikola.grabovica@htecgroup.com>
2021-12-10 10:42:38 +0100

    testing comment-pr

--- scripts/comment_pr.js
+++ scripts/comment_pr.js
@@ -1,7 +1,8 @@
 module.exports = ({github, context}) => {
     if (context.eventName == "pull_request") {
         const { issue: { number: issue_number }, repo: { owner, repo }  } = context;
-        const body = decodeURI("👋 Hey! ${{ process.env.REPO_SUMMARY }}");
+        const { REPO_SUMMARY } = process.env
+        const body = decodeURI('👋 Hey! ${REPO_SUMMARY}');
         github.rest.issues.createComment({ issue_number, owner, repo, body });
     } else {
         console.log("Not a PR")
